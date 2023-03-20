463fc7f035f278ce0e43b8c26c6f62ba9684719c
romulo max <maxromulo16@gmail.com>
2023-03-20 11:54:25 -0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
-
+import { Global_style } from './style_global'
 function App() {
   return (
     <div className="App">
+      <Global_style/>
+      {/* <img src = 'https://www.saalve.com/assets/images/logo.png'/>
+       */}
+       <h1>Hello, Word!!!</h1>
     </div>
   )
 }
---
+++ src/style_global/index.jsx
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+export const Global_style = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600&display=swap');
+   :root{
+      --color-black : black;
+      --color-gray:#656565;
+
+      --bg-primary: white;
+      --bg-blue: #0099dc;
+      --bg-orange: #0099dc
+   }
+
+   *{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+   }
+
+   *, button, input {
+    border: 0;
+    background: none;
+    font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Ubuntu, Arial, sans-serif;
+  }
+   html, body{
+      background: var(--bg-primary);
+      font-family: 'Oswald', sans-serif;
+   }
+`

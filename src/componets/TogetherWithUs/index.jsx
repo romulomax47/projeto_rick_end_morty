c6479b431061170269d592baf69bc41b9b457bb7
romulo max <maxromulo16@gmail.com>
2023-03-31 22:48:08 -0300

    add styled

--- src/componets/TogetherWithUs/index.jsx
+++ src/componets/TogetherWithUs/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import Icon_ke from '../../assets/imgs/icon-ke.png'
-import Icon_store from '../../assets/imgs/icon-shop.png'
 import { Container, Cards } from './styles';
+import Card from '../Card';
 
 function TogetherWithUs() {
    return (
@@ -9,22 +8,16 @@ function TogetherWithUs() {
          <div className='wap'>
             <h1>Junti-se ao <span>Saalve</span></h1>
             <Cards>
-               <section>
-                  <img src={Icon_ke} alt="" />
-                  <div>
-                     <h3>Registre seu Comércio</h3>
-                     <p>Descubra os benefícios dos +200 aliados da comunidade que já trabalham com a Saave</p>
-                     <button>Saiba mais</button>
-                  </div>
-               </section>
-               <section>
+               <Card icon='ke'/> 
+               <Card icon ='store'/> 
+               {/* <section>
                   <img src={Icon_store} alt="" />
                   <div>
                      <h3>Registre seu Comércio</h3>
                      <p>Descubra os benefícios dos +200 aliados da comunidade que já trabalham com a Saave</p>
                      <button>Saiba mais</button>
                   </div>
-               </section>
+               </section> */}
             </Cards>
          </div>
       </Container>
